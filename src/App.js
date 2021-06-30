3997bedb21cd724c784d3e02f063d4312d39ad9c
HAN20190621 <52053194+HAN20190621@users.noreply.github.com>
2021-06-30 13:54:56 +1000

    AddPlayer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles.css";
 //import Board from "./components/Board";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import AddPlayer from "./components/AddPlayer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //https://reactrouter.com/web/example/no-match
@@ -11,6 +12,7 @@ export default function App() {
   return (
     <Router>
       <div className="App">
+        <AddPlayer />
         <Switch>
           <Route exact path="/" component={Footer} />
           <Route path="/about" component={About} />
