3fa86b51cc5a9b2acda7eae47d3517e2f15b5de5
HAN20190621 <52053194+HAN20190621@users.noreply.github.com>
2021-07-16 18:14:00 +1000

    resiing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,21 @@ import { useEffect, useRef, useState } from "react";
 const Header = ({ title, resizing }) => {
   const location = useLocation();
   const h1Ref = useRef(null);
-  const [minWidth, setMinWidth] = useState(0);
-  //const [style, setStyle] = useState({ marginLeft: "0" });
+  const divRef = useRef(null);
+  const [style, setStyle] = useState({ marginLeft: "50px" });
 
   useEffect(() => {
     let rect1 = h1Ref.current.getBoundingClientRect();
-    setMinWidth((prevValue) => {
-      if (prevValue === 0) return rect1.width;
-    });
-  }, []);
+    let rect2 = divRef.current.getBoundingClientRect();
+    setStyle({ marginLeft: rect1.bottom === rect2.top ? "0px" : "50px" });
+  }, [resizing]);
 
-  useEffect(() => {
-    let rect1 = h1Ref.current.getBoundingClientRect();
-    console.log(rect1.height + " " + minWidth);
-  }, [resizing, minWidth]);
+  //setInterval(repos, 100);
 
   return (
     <div className="header">
       <h1 ref={h1Ref}>{title}</h1>
-      <div>
+      <div ref={divRef} style={style}>
         {location.pathname === "/" && (
           <Button text="Start" colour="green" onClick={() => {}} />
         )}
